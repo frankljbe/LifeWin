83238c078525f313e40db19faeae0ca909bd22e7
frankljbe <frankljbe@gmail.com>
2015-05-10 14:54:41 -0400

    Added goals activity and title bar.
    Need to make title bar scale based on screen size.

--- app/src/main/java/co/lifewin/andriod/MainGoals.java
+++ app/src/main/java/co/lifewin/andriod/MainGoals.java
@@ -2,51 +2,30 @@ package co.lifewin.andriod;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
-import android.widget.TextView;
 
 
-public class MainMenu extends Activity {
+public class MainGoals extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
+        setContentView(R.layout.activity_main_goals);
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        setupUserView();
-    }
-
-    private void setupUserView()
-    {
-        SharedPreferences userSettings = getSharedPreferences(Constants.UserSettings.UserSettingsRoot, MODE_PRIVATE);
-        String userName = userSettings.getString(Constants.UserSettings.UserNameKey, "");
-        TextView userNameDisplay = (TextView) findViewById(R.id.user_name);
-        userNameDisplay.setText(userName);
-    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_menu_items, menu);
+        getMenuInflater().inflate(R.menu.menu_main_goals, menu);
         return true;
     }
 
-    public void goToEditNameActivity(View view) {
-        Intent intent = new Intent(this, EditUserInfoPage.class);
-        startActivity(intent);
-    }
-
-    public void goToGoalsActivity(View view) {
-        Intent intent = new Intent(this, MainGoals.class);
+    public void goToMainMenu(View view) {
+        Intent intent = new Intent(this, MainMenu.class);
         startActivity(intent);
     }
 
